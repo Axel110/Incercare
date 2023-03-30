a87258d8ddaa355968dfa968e11357e5e79b60ba
axel_alex <alexandru.munteanu@student.tuiasi.ro>
2023-03-30 21:19:15 +0300

    hmm

---
+++ Incercare2/src/main/java/Incercare/Incercare2/App.java
@@ -0,0 +1,13 @@
+package Incercare.Incercare2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
